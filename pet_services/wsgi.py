82ce2ae7399421e1283316a21c87021ed534ed5a
Kateryna Zadorozhna <katya.zadorozhna.00@gmail.com>
2023-10-01 17:43:24 +0300

    Add new app vet_clinics

--- pet_services/wsgi.py
+++ pet_services/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pet_services.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pet_services.settings")
 
 application = get_wsgi_application()
